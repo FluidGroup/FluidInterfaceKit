acbf1bdd4f597a6ae4248a08c93b9040919956ae
Muukii <muukii.app@gmail.com>
2022-02-11 00:16:47 +0900

    Fix find logic

--- Sources/FluidInterfaceKit/Helper/UIViewController+.swift
+++ Sources/FluidInterfaceKit/Helper/UIViewController+.swift
@@ -10,7 +10,7 @@ extension UIViewController {
   public func fluidStackControllers() -> [FluidStackController] {
 
     return sequence(first: self) {
-      $0.parent
+      $0.next
     }
     .compactMap { $0 as? FluidStackController }
 
