1ceef676b8adf45a8d81abb14709ea3e1daf6b96
Muukii <muukii.app@gmail.com>
2022-02-12 00:00:04 +0900

    Format

--- Sources/FluidInterfaceKitTests/FluidStackControllerTests.swift
+++ Sources/FluidInterfaceKitTests/FluidStackControllerTests.swift
@@ -1,55 +1,70 @@
-
+import FluidInterfaceKit
 import Foundation
 import XCTest
-import FluidInterfaceKit
 
 final class FluidStackControllerTests: XCTestCase {
-  
+
   func testAddingRemoving() {
-    
+
     let stack = FluidStackController()
-    
+
     XCTAssertEqual(stack.stackingViewControllers.count, 0)
-    
+
     stack.addContentViewController(UIViewController(), transition: .noAnimation)
-    
+
     XCTAssertEqual(stack.stackingViewControllers.count, 1)
-    
+
     stack.addContentViewController(UIViewController(), transition: .noAnimation)
-    
+
     XCTAssertEqual(stack.stackingViewControllers.count, 2)
-    
+
     stack.removeLastViewController(transition: .noAnimation)
-        
+
     XCTAssertEqual(stack.stackingViewControllers.count, 1)
-    
+
     stack.removeLastViewController(transition: .noAnimation)
-        
+
     XCTAssertEqual(stack.stackingViewControllers.count, 0)
   }
-  
+
   func testAddingRemovingWithRetainRoot() {
-    
+
     let stack = FluidStackController(configuration: .init(retainsRootViewController: true))
-    
+
     XCTAssertEqual(stack.stackingViewControllers.count, 0)
-    
+
     stack.addContentViewController(UIViewController(), transition: .noAnimation)
-    
+
     XCTAssertEqual(stack.stackingViewControllers.count, 1)
-    
+
     stack.addContentViewController(UIViewController(), transition: .noAnimation)
-    
+
     XCTAssertEqual(stack.stackingViewControllers.count, 2)
-    
+
     stack.removeLastViewController(transition: .noAnimation)
-        
+
     XCTAssertEqual(stack.stackingViewControllers.count, 1)
-    
+
     // won't remove root view controller
     stack.removeLastViewController(transition: .noAnimation)
-        
+
     XCTAssertEqual(stack.stackingViewControllers.count, 1)
   }
-  
+
+  func testFindContext() {
+
+    let stack = FluidStackController()
+
+    let controller = UIViewController()
+
+    let wrapper = FluidWrapperViewController(
+      content: .init(bodyViewController: controller, view: nil)
+    )
+
+    stack.addContentViewController(wrapper, transition: .noAnimation)
+
+    XCTAssertNotNil(controller.fluidStackContext)
+
+  }
+
 }
