ac1eb85ed7c9af99bc65eed0f2a06a9f007e30bc
Muukii <muukii.app@gmail.com>
2022-02-20 18:03:14 +0900

    :evergreen_tree: Update

--- Package.swift
+++ Package.swift
@@ -1,20 +1,41 @@
 // swift-tools-version:5.5
 import PackageDescription
 
+
+
 let package = Package(
   name: "FluidInterfaceKit",
   platforms: [
-    .iOS(.v12),
+    .iOS(.v12)
   ],
   products: [
     .library(name: "FluidInterfaceKit", targets: ["FluidInterfaceKit"]),
     .library(name: "FluidInterfaceKitRideauSupport", targets: ["FluidInterfaceKitRideauSupport"]),
   ],
   dependencies: [
-    .package(name: "MatchedTransition", url: "https://github.com/muukii/MatchedTransition", .upToNextMajor(from: "1.1.0")),
-    .package(name: "GeometryKit", url: "https://github.com/muukii/GeometryKit", .upToNextMajor(from: "1.1.0")),
-    .package(name: "ResultBuilderKit", url: "https://github.com/muukii/ResultBuilderKit.git", .upToNextMajor(from: "1.1.0")),
-    .package(name: "Rideau", url: "https://github.com/muukii/Rideau.git", .upToNextMajor(from: "2.1.0"))
+    .package(
+      name: "MatchedTransition",
+      url: "https://github.com/muukii/MatchedTransition",
+      .upToNextMajor(from: "1.1.0")
+    ),
+    .package(
+      name: "GeometryKit",
+      url: "https://github.com/muukii/GeometryKit",
+      .upToNextMajor(from: "1.1.0")
+    ),
+    .package(
+      name: "ResultBuilderKit",
+      url: "https://github.com/muukii/ResultBuilderKit.git",
+      .upToNextMajor(from: "1.1.0")
+    ),
+    .package(
+      name: "Rideau",
+      url: "https://github.com/muukii/Rideau.git",
+      .upToNextMajor(from: "2.1.0")
+    ),
+
+//    .package(url: "https://github.com/apple/swift-docc-plugin", branch: "main"),
+    
   ],
   targets: [
     .target(
@@ -24,7 +45,6 @@ let package = Package(
     .target(
       name: "FluidInterfaceKitRideauSupport",
       dependencies: ["FluidInterfaceKit", "Rideau"]
-    )
+    ),
   ]
 )
-
