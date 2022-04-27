1ca0d433fbca0846c777be978641e980c764506d
Muukii <muukii.app@gmail.com>
2022-04-27 18:13:28 +0900

    Update demo

--- Sources/FluidInterfaceKit-Demo/DemoSheetViewController.swift
+++ Sources/FluidInterfaceKit-Demo/DemoSheetViewController.swift
@@ -1,4 +1,3 @@
-
 import CompositionKit
 import FluidInterfaceKit
 import FluidInterfaceKitRideauSupport
@@ -8,23 +7,72 @@ import StorybookKit
 import UIKit
 
 final class DemoSheetViewController: FluidStackController {
-  
+
   init() {
     super.init(rootViewController: ListViewController())
   }
-  
+
 }
 
 private final class ListViewController: CodeBasedViewController {
-  
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    
+
     view.backgroundColor = .systemBackground
+
+    Mondrian.buildSubviews(on: view) {
+      
+      ZStackBlock {
+        VStackBlock {
+          Components.makeSelection()&>.do {
+            $0.handlers.onTap = { [unowned self] in
+              print("hey")
+            }
+          }
+        }
+      }
+      
+    }
+  }
+
+  private enum Components {
+
+    static func makeSelection() -> InteractiveView<UIView> {
+
+      return InteractiveView(
+        animation: .bodyShrink,
+        haptics: .impactOnTouchUpInside(),
+        useLongPressGesture: false,
+        contentView: AnyView { _ in
+
+          VStackBlock {
+            
+            UILabel()&>.do {
+              $0.text = "💡 Light bulb"
+              $0.font = UIFont.boldSystemFont(ofSize: 16)
+            }
+            .viewBlock
+            .padding(10)
+            .background(ShapeLayerView.roundedCorner(radius: 6)&>.do {
+              $0.shapeFillColor = .init(white: 0.5, alpha: 0.3)
+            })
+            
+          }
+          
+        }
+      )
+
+    }
+
   }
 }
 
 private final class DetailViewController: CodeBasedViewController {
-  
-  
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    view.backgroundColor = .neon(.purple)
+  }
 }
