84fc3d3f53c0f9d889068fc68c3f876afd5ba389
Muukii <muukii.app@gmail.com>
2022-09-21 02:29:58 +0900

    :evergreen_tree: Update

--- Package.swift
+++ Package.swift
@@ -13,22 +13,22 @@ let package = Package(
   dependencies: [
     .package(
       name: "MatchedTransition",
-      url: "https://github.com/muukii/MatchedTransition",
+      url: "https://github.com/FluidGroup/MatchedTransition",
       .upToNextMajor(from: "1.1.0")
     ),
     .package(
       name: "GeometryKit",
-      url: "https://github.com/muukii/GeometryKit",
+      url: "https://github.com/FluidGroup/GeometryKit",
       .upToNextMajor(from: "1.1.0")
     ),
     .package(
       name: "ResultBuilderKit",
-      url: "https://github.com/muukii/ResultBuilderKit.git",
+      url: "https://github.com/FluidGroup/ResultBuilderKit.git",
       .upToNextMajor(from: "1.2.0")
     ),
     .package(
       name: "Rideau",
-      url: "https://github.com/muukii/Rideau.git",
+      url: "https://github.com/FluidGroup/Rideau.git",
       .upToNextMajor(from: "2.1.0")
     ),
     .package(url: "https://github.com/apple/swift-docc-plugin.git", branch: "main"),    
