80cd7b5bf774ec3b53603ec6d8e16b43b7556fe2
Hiroshi Kimura <muukii.app@gmail.com>
2024-03-26 01:36:23 +0900

    Add ContextMenu support for FluidStack (#174)

---
+++ Sources/FluidStack/ContextMenu/ContextMenuSupport.swift
@@ -0,0 +1,107 @@
+import UIKit
+
+/**
+ It runs standalone, without using delegate.
+ Passing any actions distinguished each interaction
+ */
+public final class StandaloneContextMenuInteraction: UIContextMenuInteraction {
+
+  private let proxy: _InteractionDelegateProxy
+
+  public init(
+    makeConfiguration: @escaping (CGPoint) -> UIContextMenuConfiguration,
+    willPerformPreviewAction: @escaping @MainActor (
+      UIContextMenuConfiguration, any UIContextMenuInteractionCommitAnimating
+    ) -> Void
+  ) {
+
+    let proxy = _InteractionDelegateProxy(
+      makeConfiguration: makeConfiguration,
+      willPerformPreviewAction: willPerformPreviewAction
+    )
+    self.proxy = proxy
+    super.init(delegate: proxy)
+  }
+
+  /**
+   Convenience inititalizer for FluidStackController
+   */
+  public init(
+    targetStackController: FluidStackController,
+    destinationViewController: @escaping @MainActor () -> FluidViewController
+  ) {
+
+    let proxy = _InteractionDelegateProxy(
+      makeConfiguration: {
+        location in
+        UIContextMenuConfiguration(
+          identifier: nil,
+          previewProvider: {
+            let destination = destinationViewController()
+            return destination
+          },
+          actionProvider: { _ in
+            return UIMenu(title: "", children: [])
+          }
+        )
+      },
+      willPerformPreviewAction: {
+        [weak targetStackController] configuration,
+        animator in
+        
+        guard let targetStackController else {
+          return
+        }
+        
+        animator.addCompletion {
+          targetStackController.fluidPush(
+            animator.previewViewController as! FluidViewController,
+            target: .current,
+            relation: .modality,
+            transition: nil,
+            completion: nil
+          )
+        }
+
+      }
+    )
+    self.proxy = proxy
+    super.init(delegate: proxy)
+
+  }
+
+}
+
+private final class _InteractionDelegateProxy: NSObject, UIContextMenuInteractionDelegate {
+
+  private let makeConfiguration: @MainActor (CGPoint) -> UIContextMenuConfiguration
+  private let willPerformPreviewAction:
+  @MainActor (UIContextMenuConfiguration, any UIContextMenuInteractionCommitAnimating) -> Void
+
+  init(
+    makeConfiguration: @escaping @MainActor (CGPoint) -> UIContextMenuConfiguration,
+    willPerformPreviewAction: @escaping @MainActor (
+      UIContextMenuConfiguration, any UIContextMenuInteractionCommitAnimating
+    ) -> Void
+  ) {
+    self.makeConfiguration = makeConfiguration
+    self.willPerformPreviewAction = willPerformPreviewAction
+  }
+
+  func contextMenuInteraction(
+    _ interaction: UIContextMenuInteraction,
+    configurationForMenuAtLocation location: CGPoint
+  ) -> UIContextMenuConfiguration? {
+
+    makeConfiguration(location)
+  }
+
+  func contextMenuInteraction(
+    _ interaction: UIContextMenuInteraction,
+    willPerformPreviewActionForMenuWith configuration: UIContextMenuConfiguration,
+    animator: any UIContextMenuInteractionCommitAnimating
+  ) {
+    willPerformPreviewAction(configuration, animator)
+  }
+
+}
