fa38a73c0199033a2ea4f6ee66934217e5eff5b4
Muukii <muukii.app@gmail.com>
2024-10-17 08:28:01 +0900

    Patch

--- Sources/FluidSnackbar/FloatingDisplayTransitionType.swift
+++ Sources/FluidSnackbar/FloatingDisplayTransitionType.swift
@@ -1,10 +1,11 @@
 
 import UIKit
 
-@MainActor
 public protocol FloatingDisplayTransitionType {
 
+  @MainActor
   func applyPresentAnimation(notificationView: FloatingDisplayViewType, completion: @escaping () -> Void)
 
+  @MainActor
   func applyDismissAnimation(notificationView: FloatingDisplayViewType, completion: @escaping () -> Void)
 }
